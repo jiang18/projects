17682b86f4bfd0020f09b60d7ad03adc2bc38508
Jicai Jiang <jiang18@terpmail.umd.edu>
2020-12-07 17:23:51 -0500

    Update qqman.R

--- instruction/qqman.R
+++ instruction/qqman.R
@@ -9,7 +9,9 @@ manhattan(gwas, chr = "CHR", bp = "POS", p = "SNP_TSCORE_PVAL", snp = "SNPNAME",
 colnames(gwas)[c(1,4,15)] = c("SNP","BP","P")
 manhattan(gwas, genomewideline = -log10(0.05/nrow(gwas)))
 
-# If not interactive, save the plot in a PDF file
-pdf(filename)
+# Interactice if X11-forwarding is enabled.
+# How to enable X11-forwarding in PuTTY: http://ocean.stanford.edu/courses/ESS141/PuTTY/
+# Alternative, save the plot in a PDF file
+pdf("filename")
 manhattan(gwas, genomewideline = -log10(0.05/nrow(gwas)))
 dev.off()
