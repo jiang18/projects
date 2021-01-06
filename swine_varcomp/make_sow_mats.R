4eeddd583f2c3ec4116e0b04b4a64bae5e82afe8
Jicai Jiang <jiang18@terpmail.umd.edu>
2021-01-06 10:19:01 -0500

    Update make_sow_mats.R

--- swine_varcomp/make_sow_mats.R
+++ swine_varcomp/make_sow_mats.R
@@ -9,3 +9,16 @@ write.table(sow.imat, file="yorkshire.sows.i.txt", quote = FALSE, sep = " ",row.
 sow.amat = M %*% as.matrix(sow.grm) %*% t(M)
 write.table(sow.amat, file="yorkshire.sows.a.txt", quote = FALSE, sep = " ",row.names = TRUE,col.names = TRUE)
 
+################################################################################
+sow.grm = read.table("yorkshire.sows.dom.grm.txt", head=T, check.names=F)
+link = read.table("yorkshire.sows",head=T)
+sow.factor = factor(link$sow, levels = rownames(sow.grm))
+M = table(link$id, sow.factor)
+
+sow.dmat = M %*% as.matrix(sow.grm) %*% t(M)
+write.table(sow.dmat, file="yorkshire.sows.d.txt", quote = FALSE, sep = " ",row.names = TRUE,col.names = TRUE)
+
+sow.grm = read.table("yorkshire.sows.foi.grm.txt", head=T, check.names=F)
+sow.fmat = M %*% as.matrix(sow.grm) %*% t(M)
+write.table(sow.fmat, file="yorkshire.sows.f.txt", quote = FALSE, sep = " ",row.names = TRUE,col.names = TRUE)
+
