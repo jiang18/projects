2cab972cde473b3d02995a9abce7865749cff0a7
Jicai Jiang <jiang18@terpmail.umd.edu>
2021-01-06 12:52:56 -0500

    Update plot.R

--- swine_varcomp/plot.R
+++ swine_varcomp/plot.R
@@ -1,11 +1,22 @@
+par(mfrow=c(2,1))
+
 d = read.table("yd.yorkshire.BirthWeight.variance.components.T.csv",sep=",")
 idx = seq(13,45,by=4)
 lbls = c("A","F","D","MI","MA","MD","MF","Litter","E")
 slices = as.numeric(d[idx,2])
-pie(slices, labels=lbls, main="Birth Weight")
 pct = round(slices*100)
 lbls = paste(lbls,pct)
 lbls = paste(lbls,"%",sep="")
+pie(slices, labels=lbls, main="Birth Weight")
+
+d = read.table("yd.yorkshire.AverageDailyGain_Nursery.variance.components.T.csv",sep=",")
+idx = seq(13,45,by=4)
+lbls = c("A","F","D","MI","MA","MD","MF","Litter","E")
+slices = as.numeric(d[idx,2])
+pct = round(slices*100)
+lbls = paste(lbls,pct)
+lbls = paste(lbls,"%",sep="")
+pie(slices, labels=lbls, main="Average Dairy Gain Nursery")
 
 dev.off()
 
