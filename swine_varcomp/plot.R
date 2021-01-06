6734ad77941180c8661e5ac5e1aa7c2c31667434
Jicai Jiang <jiang18@terpmail.umd.edu>
2021-01-06 12:06:02 -0500

    Create plot.R

---
+++ swine_varcomp/plot.R
@@ -0,0 +1,11 @@
+d = read.table("yd.yorkshire.BirthWeight.variance.components.T.csv",sep=",")
+idx = seq(13,45,by=4)
+lbls = c("A","F","D","MI","MA","MD","MF","Litter","E")
+slices = as.numeric(d[idx,2])
+pie(slices, labels=lbls, main="Birth Weight")
+pct = round(slices*100)
+lbls = paste(lbls,pct)
+lbls = paaste(lbls,"%",sep="")
+
+dev.off()
+
