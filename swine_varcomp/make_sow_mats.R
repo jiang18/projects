9de9d0653c6022e827b4110574fc0c3635825dc0
Jicai Jiang <jiang18@terpmail.umd.edu>
2021-01-05 23:18:13 -0500

    Create make_sow_mats.R

---
+++ swine_varcomp/make_sow_mats.R
@@ -0,0 +1,11 @@
+sow.grm = read.table("yorkshire.sows.add.grm.txt", head=T, check.names=F)
+link = read.table("yorkshire.sows",head=T)
+sow.factor = factor(link$sow, levels = rownames(sow.grm))
+M = table(link$id, sow.factor)
+
+sow.imat = M %*% t(M)
+write.table(sow.imat, file="yorkshire.sows.i.txt", quote = FALSE, sep = " ",row.names = TRUE,col.names = TRUE)
+
+sow.amat = M %*% as.matrix(sow.grm) %*% t(M)
+write.table(sow.amat, file="yorkshire.sows.a.txt", quote = FALSE, sep = " ",row.names = TRUE,col.names = TRUE)
+
