4525e965dc4a6f90c94aea3463c01b16493e8ae8
Jicai Jiang <jiang18@terpmail.umd.edu>
2021-03-29 15:20:21 -0400

    Update funct_boxplot.R

--- mph/funct_boxplot.R
+++ mph/funct_boxplot.R
@@ -1,5 +1,10 @@
-par(mar=)
-d = read.table("50k.summary.txt",head=T)
-boxplot(d,las=2)
+true = read.table("sim_true.txt")
+dt = read.table("50k.summary.txt",head=T)
+names(dt) = true[,1]
+
+boxplot(dt,las=2)
+points(1:ncol(dt), colMeans(dt), col = "red", pch=1)
+points(1:ncol(dt), true[,2], col = "blue", pch=2)
 abline(h=1,col="blue")
 dev.off()
+
