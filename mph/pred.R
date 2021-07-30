5012f80f281b2ef69ce1c33f6016265993b086ec
Jicai Jiang <jiang18@terpmail.umd.edu>
2021-07-30 15:44:09 -0400

    Update pred.R

--- mph/pred.R
+++ mph/pred.R
@@ -1,19 +1,3 @@
-true = read.csv("hsq50.sim.csv")
-nn = 10
-cor = matrix(nrow=nn, ncol=3)
-for(i in 1:nn) {
-  one = read.csv( paste("hsq50", i, "one.mq.gv.csv", sep=".") )
-  cor[i,1] = cor(true[9001:10000,i+101], one[9001:10000,2])
-  main = read.csv( paste("hsq50", i, "main.mq.gv.csv", sep=".") )
-  cor[i,2] = cor(true[9001:10000,i+101], main[9001:10000,2])
-  main = read.csv( paste("hsq50", i, "sig.mq.gv.csv", sep=".") )
-  cor[i,3] = cor(true[9001:10000,i+101], main[9001:10000,2])
-}
-t.test(cor[,1],cor[,3], paired=T)
-
-d = read.csv( paste("hsq50", i, "loo.mq.vc.csv", sep=".") )
-d$vc[which(abs(d$var /d$seV) > 3)]
-
 ###############################
 
 true = read.csv("pheno/hsq90.gv.sim.csv")
@@ -31,15 +15,16 @@ t.test(cor[,1],cor[,2], paired=T)
 ################################
 true = read.csv("hsq50.sim.csv")
 nn = 10
-cor = matrix(nrow=nn, ncol=4)
+cor = matrix(nrow=nn, ncol=3)
 for(i in 1:nn) {
   one = read.csv( paste("hsq50", i, "one.mq.gv.csv", sep=".") )
   cor[i,1] = cor(true[9001:10000,i+101], one[9001:10000,2])
-  main = read.csv( paste("hsq50", i, "main.mq.gv.csv", sep=".") )
+  main = read.csv( paste("hsq50", i, "loo.mq.gv.csv", sep=".") )
   cor[i,2] = cor(true[9001:10000,i+101], main[9001:10000,2])
-  main = read.csv( paste("hsq50", i, "step.mq.gv.csv", sep=".") )
-  cor[i,3] = cor(true[9001:10000,i+101], main[9001:10000,2])
-  main = read.csv( paste("hsq50", i, "sig.mq.gv.csv", sep=".") )
-  cor[i,4] = cor(true[9001:10000,i+101], main[9001:10000,2])
+  main = read.csv( paste("hsq50", i, "top.mq.gv.csv", sep=".") )
+  cor[i,2] = cor(true[9001:10000,i+101], main[9001:10000,2])
 }
 t.test(cor[,1],cor[,3], paired=T)
+
+d = read.csv( paste("hsq50", i, "loo.mq.vc.csv", sep=".") )
+d$vc[which(abs(d$var /d$seV) > 3)]
