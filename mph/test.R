ead255595af5a853efe4a08a84fe8afdf6cc1315
Jicai Jiang <jiang18@terpmail.umd.edu>
2021-07-31 00:42:15 -0400

    Create test.R

---
+++ mph/test.R
@@ -0,0 +1,30 @@
+library(data.table)
+sw = fread("../funct.snp_info.csv", head=T, sep=",")
+
+rname = sw$snp
+sw = sw[,-1]
+sw[is.na(sw)] = 0
+sw = as.matrix(sw)
+
+main = which( !(grepl("500", colnames(sw)) | grepl("peak", colnames(sw))) )
+sw = sw[,main]
+
+rownames(sw) = rname
+
+sw = sw[,-23]
+# MPH output
+mq = read.csv("../10k/hsq50.1.loo.mq.vc.csv")
+mq = mq[-nrow(mq),]
+
+# heritability estimate
+lhs = t(sw) %*% sw
+var = lhs %*% as.matrix(mq[,9:ncol(mq)]) %*% t(lhs)
+h.est = lhs %*% mq$enrichment / sum(mq$m)
+h.se = sqrt(diag(var)) / sum(mq$m) 
+
+# enrichment estimate
+e.est = h.est / (mq$m/mq$m[1])
+e.se = h.se / (mq$m/mq$m[1])
+
+hh = cbind(h.est, h.se, e.est, e.se)
+hh[order(hh[,3]),]
