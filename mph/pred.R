c0d2bf46ed5566b35cabde60610e2047ec2e023a
Jicai Jiang <jiang18@terpmail.umd.edu>
2021-07-31 02:52:47 -0400

    Update pred.R

--- mph/pred.R
+++ mph/pred.R
@@ -1,12 +1,14 @@
 true = read.csv("pheno/hsq90.gv.sim.csv")
 
-nn = 10
-cor = matrix(nrow=nn, ncol=2)
+nn = 1
+cor = matrix(nrow=nn, ncol=3)
 for(i in 1:nn) {
   one = read.csv( paste("50k/hsq90", i, "one.mq.gv.csv", sep=".") )
   cor[i,1] = cor(true[45001:50000,i+101], one[45001:50000,2])
   sig = read.csv( paste("50k/hsq90", i, "sig.mq.gv.csv", sep=".") )
   cor[i,2] = cor(true[45001:50000,i+101], sig[45001:50000,2])
+  sig = read.csv( paste("50k/hsq90", i, "top.mq.gv.csv", sep=".") )
+  cor[i,3] = cor(true[45001:50000,i+101], sig[45001:50000,2])
 }
 t.test(cor[,1],cor[,2], paired=T)
 
