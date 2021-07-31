78c6bb5609b7c9c63d9ab5f00797b174aae258dd
Jicai Jiang <jiang18@terpmail.umd.edu>
2021-07-31 15:52:01 -0400

    Update test.R

--- mph/test.R
+++ mph/test.R
@@ -13,8 +13,12 @@ rownames(sw) = rname
 
 sw = sw[,-23]
 # MPH output
-mq = read.csv("../10k/hsq50.1.loo.mq.vc.csv")
+mq = read.csv("../10k/hsq50.2.loo.mq.vc.csv")
 mq = mq[-nrow(mq),]
+
+snpvar = sw %*% mq$enrichment * (nrow(sw)/sum(mq$m))
+
+######################################################
 
 # heritability estimate
 lhs = t(sw) %*% sw
@@ -28,3 +32,25 @@ e.se = h.se / (mq$m/mq$m[1])
 
 hh = cbind(h.est, h.se, e.est, e.se)
 hh[order(hh[,3]),]
+
+##############
+# 50K
+##############
+sw = fread("../funct.snp_info.csv", head=T, sep=",")
+
+rname = sw$snp
+sw = sw[,-1]
+sw[is.na(sw)] = 0
+sw = as.matrix(sw)
+
+main = which( !(grepl("500", colnames(sw)) | grepl("peak", colnames(sw))) )
+sw = sw[,main]
+
+rownames(sw) = rname
+
+# MPH output
+mq = read.csv("../50k/hsq90.2.200.1.mq.csv")
+mq = mq[-nrow(mq),]
+
+snpvar = sw %*% mq$enrichment * (nrow(sw)/sum(mq$m))
+
