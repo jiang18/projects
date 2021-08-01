b5db04efc7e3243f4a5d742482d9237df0fd4576
Jicai Jiang <jiang18@terpmail.umd.edu>
2021-08-01 11:57:46 -0400

    Update pred.R

--- mph/pred.R
+++ mph/pred.R
@@ -1,14 +1,16 @@
 true = read.csv("pheno/hsq90.gv.sim.csv")
 
-nn = 1
-cor = matrix(nrow=nn, ncol=3)
+nn = 10
+cor = matrix(nrow=nn, ncol=4)
 for(i in 1:nn) {
   one = read.csv( paste("50k/hsq90", i, "one.mq.gv.csv", sep=".") )
   cor[i,1] = cor(true[45001:50000,i+101], one[45001:50000,2])
+  one = read.csv( paste("50k/hsq90", i, "main.mq.gv.csv", sep=".") )
+  cor[i,2] = cor(true[45001:50000,i+101], one[45001:50000,2])
   sig = read.csv( paste("50k/hsq90", i, "sig.mq.gv.csv", sep=".") )
-  cor[i,2] = cor(true[45001:50000,i+101], sig[45001:50000,2])
-  sig = read.csv( paste("50k/hsq90", i, "top.mq.gv.csv", sep=".") )
   cor[i,3] = cor(true[45001:50000,i+101], sig[45001:50000,2])
+  sig = read.csv( paste("50k/hsq90", i, "top.mq.gv.csv", sep=".") )
+  cor[i,4] = cor(true[45001:50000,i+101], sig[45001:50000,2])
 }
 t.test(cor[,1],cor[,2], paired=T)
 
@@ -26,6 +28,6 @@ for(i in 1:nn) {
 }
 t.test(cor[,1],cor[,3], paired=T)
 
-i = 1
-xxx = read.csv( paste("hsq50", i, "sig.mq.gv.csv", sep=".") )
+i = 2
+xxx = read.csv( paste("hsq50", i, "funct.mq.gv.csv", sep=".") )
 cor(true[9001:10000,i+101], xxx[9001:10000,2])
