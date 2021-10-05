c90c0f66085003615af6f051e4c56b3737271b1f
Jian (Mark) Cheng <39278467+Markjiancheng@users.noreply.github.com>
2021-10-04 23:52:46 -0400

    Add files via upload

--- swine_varcomp/make_sow_i_mat.R
+++ swine_varcomp/make_sow_i_mat.R
@@ -1,14 +1,16 @@
 link = read.table("pedigree.file",head=F)
 link<- link[,c(2,4)]
 names(link)<- c("ID","Sow")
-ind<- read.table("SPG_ped.fam", header = F)
-ind<- as.data.frame(ind[,c(2)])
+ind<- read.table("SPG_QC.grm.indi", header = F)
 names(ind)<- "ID"
+ind$order<- c(1:nrow(ind))
 link<- merge(ind, link, by="ID")
+link<- link[order(link$order),]
+
 
 lt.factor = factor(link$Sow)
-M = table(link$ID, lt.factor)
+M = table(link$order, lt.factor)
 
 lt.imat = as.data.frame(M %*% t(M))
-write.table(lt.imat, file="SPG.sow.i.txt", quote = FALSE, sep = " ",row.names = TRUE,col.names = TRUE)
+write.table(lt.imat, file="SPG.sow.i.txt", quote = FALSE, sep = " ",row.names = link$ID,col.names = link$ID)
 #fwrite(lt.imat, file="SPG.sow.i.txt", quote = FALSE, sep = " ",row.names = TRUE,col.names = TRUE)
